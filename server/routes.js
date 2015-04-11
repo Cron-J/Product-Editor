67015815047649620a029f9c47a86e6e13fa319b
gaurav14 <gaurav@cronj.com>
2015-04-11 13:14:32 +0530

    export product csv

--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,8 @@ exports.endpoints = [
     { method: 'GET',  path: '/{somethingss*}', config: Static.get },
     { method: 'POST', path: '/createProduct', config: Product.createProduct },
     { method: 'POST', path: '/searchProduct', config: Product.searchProduct },
+    { method: 'POST', path: '/exportProduct', config: Product.exportProduct },
+    
     { method: 'GET', path: '/getProduct/{id}', config: Product.getProductById },
     { method: 'PUT', path: '/updateProduct/{id}', config: Product.updateProduct },
     { method: 'POST', path: '/createVariant', config: Variant.createVariant },
